4ffef76f36a3ea32a97fc7fd1bcf6b0479c2f268
Takayuki Yamazaki <zakizaki33@hotmail.com>
2021-08-18 09:19:10 +0900

    最新版に同期させたつもり

--- 20210324_1-video-GUI_0324.py
+++ 20210324_1-video-GUI_0324.py
@@ -4,13 +4,15 @@ import cv2
 import PIL.Image
 import PIL.ImageTk
 from tkinter import font
-# import time
+import datetime
+import time
 import shutil
 import os
-import datetime
 import threading
-# import sys
+import sys
 from multiprocessing import Process
+import logging
+logging.basicConfig(filename="test.log", level=logging.DEBUG)
 #  追記　 ここから
 #  global flag
 #  flag=0
@@ -39,16 +41,33 @@ class Application(tk.Frame):
         self.font_btn_small = font.Font(
             family="Meiryo UI", size=15, weight="bold")
 
-        self.font_lbl_bigger = font.Font(family="Meiryo UI", size=45, weight="bold")
-        self.font_lbl_big = font.Font(family="Meiryo UI", size=30, weight="bold")
-        self.font_lbl_middle = font.Font(family="Meiryo UI", size=15, weight="bold")
-        self.font_lbl_small = font.Font(family="Meiryo UI", size=12, weight="normal")
+        self.font_lbl_bigger = font.Font(
+            family="Meiryo UI", size=45, weight="bold")
+        self.font_lbl_big = font.Font(
+            family="Meiryo UI", size=30, weight="bold")
+        self.font_lbl_middle = font.Font(
+            family="Meiryo UI", size=15, weight="bold")
+        self.font_lbl_small = font.Font(
+            family="Meiryo UI", size=12, weight="normal")
 
         # ---------------------------------------------------------
         # Open the video source
         # ---------------------------------------------------------
 
         self.vcap = cv2.VideoCapture(video_source)
+        # https://note.nkmk.me/python-opencv-videocapture-file-camera/
+        logging.debug(datetime.datetime.now())
+        logging.debug(type(self.vcap))
+        logging.debug(self.vcap.isOpened())
+        if self.vcap.isOpened() is False:
+            logging.debug("There is no Camera")
+            self.var = tk.StringVar()
+            self.var.set("There is no Camera")
+            self.words1 = tk.Label(textvariable=self.var, font=("", 12))
+            self.words1.pack()
+            time.sleep(3)
+            sys.exit()
+
         self.width = self.vcap.get(cv2.CAP_PROP_FRAME_WIDTH)
         self.height = self.vcap.get(cv2.CAP_PROP_FRAME_HEIGHT)
         self.fps = int(self.vcap.get(cv2.CAP_PROP_FPS))
@@ -69,9 +88,12 @@ class Application(tk.Frame):
     def create_widgets(self):
 
         # Frame_Camera
-        self.frame_cam = tk.LabelFrame(self.master, text='Camera', font=self.font_frame)
-        self.frame_cam.place(x=10, y=10)
-        self.frame_cam.configure(width=self.width + 30, height=self.height + 50)
+        self.frame_cam = tk.LabelFrame(
+            self.master, text='Camera', font=self.font_frame)
+        self.frame_cam.place(
+            x=10, y=10)
+        self.frame_cam.configure(
+            width=self.width + 30, height=self.height + 50)
         self.frame_cam.grid_propagate(0)
 
         # Canvas
@@ -80,7 +102,8 @@ class Application(tk.Frame):
         self.canvas1.grid(column=0, row=0, padx=10, pady=10)
 
         # Frame_Button
-        self.frame_btn = tk.LabelFrame(self.master, text='Control', font=self.font_frame)
+        self.frame_btn = tk.LabelFrame(
+            self.master, text='Control', font=self.font_frame)
         self.frame_btn.place(x=10, y=550)
         self.frame_btn.configure(width=self.width + 30, height=120)
         self.frame_btn.grid_propagate(0)
@@ -88,24 +111,29 @@ class Application(tk.Frame):
         # Snapshot Button
         # https://www.shido.info/py/tkinter2.html
         # btnを”録画スタート だよ”という表示で作る
-        self.btn_snapshot = tk.Button(self.frame_btn, text='録画スタート だよ', font=self.font_btn_big)
+        self.btn_snapshot = tk.Button(
+            self.frame_btn, text='録画スタート だよ', font=self.font_btn_big)
         # ボタンが押された時に、press_snapshot_button を発動する
-        self.btn_snapshot.configure(width=20, height=1, command=self.press_snapshot_button)
+        self.btn_snapshot.configure(
+            width=15, height=1, command=self.press_snapshot_button)
         self.btn_snapshot.grid(column=0, row=0, padx=20, pady=10)
 
         # 追記ZAKI　2021-05-17　ここから　
         # label
         text1 = "録画停止中"
-        text2 = "録画実行中"
-        words1 = tk.Label(text=text1, font=("", 12))
-        words1.pack()
-        # words1.place(x=500, y=500)
+
+        self.words1 = tk.Label(text=text1, font=("", 12))
+        # words1.pack()
+        self.words1.place(x=800, y=100)
         # 追記　ここまで　
 
         # Close
-        self.btn_close = tk.Button(self.frame_btn, text='Close', font=self.font_btn_big)
-        self.btn_close.configure(width=15, height=1, command=self.press_close_button)
+        self.btn_close = tk.Button(
+            self.frame_btn, text='Close', font=self.font_btn_big)
+        self.btn_close.configure(
+            width=15, height=1, command=self.press_close_button)
         self.btn_close.grid(column=1, row=0, padx=20, pady=10)
+        logging.debug("button pushed !!!")
 
     def update(self):
         # Get a frame from the video source
@@ -120,6 +148,10 @@ class Application(tk.Frame):
         self.master.after(self.delay, self.update)
 
     def press_snapshot_button(self):
+        # ラベルにログを表示させる
+        logging.info("snapshot_button pushed!!!")
+        # self.var.set("snapshot_button pushed!!!")
+        self.words1["text"] = "snapshot_button pushed!!!"
         # 録画スタート
         # スレッドではなくて、プロセスを使うらしい？
         # https://qiita.com/ttiger55/items/5e1d5a3405d2b3ef8f40
@@ -151,7 +183,7 @@ class Application(tk.Frame):
         w = self.vcap.get(cv2.CAP_PROP_FRAME_WIDTH)
         h = self.vcap.get(cv2.CAP_PROP_FRAME_HEIGHT)
         fps = int(self.vcap.get(cv2.CAP_PROP_FPS))
-        fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')        # 動画保存時のfourcc設定（mp4用）
+        fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 
         dt_now = datetime.datetime.now()
 
@@ -161,33 +193,32 @@ class Application(tk.Frame):
             shutil.rmtree(yesterday.strftime('%Y%m%d'))
         except OSError as err:
             print("OS error: {0}".format(err))
+            logging.error("OS error: {0}".format(err))
             # print("NONONO")
+            logging.error("NONONO")
 
         # フォルダの作成
         folder_name = dt_now.strftime('%Y%m%d')
         if not os.path.isdir(folder_name):
             os.mkdir(folder_name)
 
-        video_name = folder_name + "/" + dt_now.strftime('%Y%m%d%H%M%S') + ".mp4"
+        video_name = folder_name + "/" + dt_now.strftime(
+            '%Y%m%d%H%M%S') + ".mp4"
         print(video_name)
+        logging.error(video_name)
 
         # 動画ファイルの保存
-
-        video = cv2.VideoWriter(video_name, fourcc, fps, (int(w), int(h)))  # 動画の仕様（ファイル名、fourcc, FPS, サイズ）
-
+        # 動画の仕様（ファイル名、fourcc, FPS, サイズ）
+        video = cv2.VideoWriter(video_name, fourcc, fps, (int(w), int(h)))
         # 動画の保存処理
         count = 0
         while True:
             _, frame = self.vcap.read()
-            video.write(frame)                                     # 動画を1フレームずつ保存する
+            video.write(frame)        # 動画を1フレームずつ保存する
 
             count = count + 1
             if count == (fps * 15):
                 break
-
-
-def test111():
-    print("HELLO PYTHON")
 
 
 def main():
@@ -195,12 +226,18 @@ def main():
     root = tk.Tk()
 
     # http://utisam.hateblo.jp/entry/2013/01/12/212958
-    root.state('zoomed')  # when windows
-    # root.attributes("-zoomed","1") # when Linux & Mac
+
+    import platform
+    if platform.system() == "Windows":
+        root.state('zoomed')  # when windows
+    else:
+        root.attributes("-zoomed", "1")  # when Linux & Mac
 
     app = Application(master=root)  # Inherit
     app.mainloop()
 
 
 if __name__ == "__main__":
+    logging.info("main start!!!")
     main()
+    
